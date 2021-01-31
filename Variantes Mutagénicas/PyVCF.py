e60d77230b0f777d61bd7d1b6d859b8410f053e6
pg42872 <pg42872@alunos.uminho.pt>
2021-01-31 22:14:36 +0000

    Nova pasta

--- Variantes Mutagénicas/PyVCF.py
+++ Variantes Mutagénicas/PyVCF.py
@@ -1,10 +1,11 @@
 import vcf
+from BCBio import GFF
 
-
-portuguesa = vcf.Reader(open('C:/Users/Zé Freitas/Desktop/Mestrado/Labs_Bioinf/Aula_TBL/dados/trimmomatic/ERR4157959.raw.vcf', 'r'))
+portuguesa = vcf.Reader(open('C:/Users/Zé Freitas/Desktop/Mestrado/Labs_Bioinf/Trabalho prático/scripts/Labs_Bioinf/Variantes Mutagénicas/ERR4157959.raw.vcf', 'r'))
 for record in portuguesa:
     print(record)
     
-espanhola = vcf.Reader(open('C:/Users/Zé Freitas/Desktop/Mestrado/Labs_Bioinf/Aula_TBL/dados/trimmomatic/ERR4395294.raw.vcf', 'r'))
+espanhola = vcf.Reader(open('C:/Users/Zé Freitas/Desktop/Mestrado/Labs_Bioinf/Trabalho prático/scripts/Labs_Bioinf/Variantes Mutagénicas/ERR4395294.raw.vcf', 'r'))
 for record in espanhola:
     print(record)
+    
